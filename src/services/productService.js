9a9718dba9126f40ef5a6295ac954a11dba04261
Michael Tibbetts <michael.tibbetts.1@gmail.com>
2024-11-15 16:19:44 -0500

    added forms

---
+++ src/services/productService.js
@@ -0,0 +1,24 @@
+//need index, create, update, and delete functions
+
+const index = async () => {
+
+}
+
+const create = async () => {
+    
+}
+
+const update = async () => {
+    
+}
+
+const deleteProduct = async () => {
+    
+}
+
+export { 
+    index,
+    create,
+    update,
+    deleteProduct
+}
---
+++ src/services/reviewsService.js
@@ -0,0 +1,29 @@
+//need index, create, update, and delete functions
+
+const index = async () => {
+
+}
+
+const createReview = async () => {
+    
+}
+
+const createComment = async () => {
+    
+}
+
+const update = async () => {
+    
+}
+
+const deleteReview = async () => {
+    
+}
+
+export { 
+    index,
+    createReview,
+    createComment,
+    update,
+    deleteReview
+}
