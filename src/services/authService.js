093f4cc64293086be0a1c03edd6f1cf3dcffcefb
Michael Tibbetts <michael.tibbetts.1@gmail.com>
2024-11-19 11:52:48 -0500

    updated login functionality

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,16 +1,29 @@
-const BACKEND_URL = `${import.meta.env.VITE_EXPRESS_BACKEND_URL}`;
-import axios from 'axios'
+// src/services/authService.js
+// Helper function to store and get the token
+const saveToken = (token) => localStorage.setItem('token', token);
+const getToken = () => localStorage.getItem('token');
+const clearToken = () => localStorage.removeItem('token');
+const BACKEND_URL = `${import.meta.env.VITE_EXPRESS_BACKEND_URL}`
+console.log('BACKEND_URL:', BACKEND_URL)
+
+const getUser = () => {
+  const token = localStorage.getItem('token');
+  if (!token) return null;
+  const user = JSON.parse(atob(token.split('.')[1]));
+  return user;
+}
 
 const signup = async (formData) => {
   try {
-    const res = await fetch(`${BACKEND_URL}/signup`, {
+    const res = await fetch(`${BACKEND_URL}/users/signup`, {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify(formData),
-      credentials: 'include', // Include cookies in the request
     });
     const json = await res.json();
-    if (json.err) throw new Error(json.err);
+    if (json.err) {
+      throw new Error(json.err);
+    }
     return json;
   } catch (err) {
     console.log(err);
@@ -24,10 +37,11 @@ const signin = async (formData) => {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify(formData),
-      credentials: 'include', // Include cookies in the request
     });
     const json = await res.json();
     if (json.error) throw new Error(json.error);
+
+    saveToken(json.token);
     return json;
   } catch (err) {
     console.log('Signin Error:', err);
@@ -35,27 +49,13 @@ const signin = async (formData) => {
   }
 };
 
+
+
 // Function to sign out a user
-const signout = async () => {
-  try {
-    await fetch(`${BACKEND_URL}/users/signout`, {
-      method: 'POST',
-      credentials: 'include',
-    });
-    console.log('User signed out successfully');
-  } catch (err) {
-    console.log('Signout Error:', err);
-  }
+const signout = () => {
+  clearToken();
+  console.log('User signed out successfully');
 };
-
-const getUser = () => {
-  const token = localStorage.getItem('token');
-  if (!token) return null;
-  const user = JSON.parse(atob(token.split('.')[1]));
-  return user;
-}
-
-
 
 // Function to check if user is authenticated
 const isAuthenticated = () => !!getToken();
